b33c588f96c3c4efdefe859ded39410bbd379f29
RavanaGurbanova8 <ravana.gurbanova17@gmail.com>
2025-08-14 16:03:25 +0400

    updated yehu

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,5 +1,28 @@
 import joblib
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import RobustScaler,  StandardScaler, MinMaxScaler
+from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.base import BaseEstimator
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
+from category_encoders import CatBoostEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from pandas import Series, DataFrame
+from xgboost import XGBClassifier
+from scipy import stats
+from sklearn.metrics import accuracy_score, confusion_matrix, mean_squared_error, r2_score, mean_absolute_error
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold, cross_validate
+
+
+
+
 
 def main():
     model = joblib.load("model.pkl")
